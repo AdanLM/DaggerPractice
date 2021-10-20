cdc0570ed1d83f78dd381677d0cbe0fc252aacdb
AdanLM <adanmanzo94@gmail.com>
2021-10-19 19:06:26 -0500

    Add Logout Menu

--- app/src/main/java/com/adanlm/daggerpractice/ui/main/MainActivity.java
+++ app/src/main/java/com/adanlm/daggerpractice/ui/main/MainActivity.java
@@ -1,6 +1,11 @@
 package com.adanlm.daggerpractice.ui.main;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 
 import com.adanlm.daggerpractice.BaseActivity;
 import com.adanlm.daggerpractice.R;
@@ -13,5 +18,22 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logout:
+                sesionManager.logOut();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
